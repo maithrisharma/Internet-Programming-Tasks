884e07c7488de7d1be9fd8724e645395d6803c0b
Maithri Sharma <maithribharadwaj3@gmail.com>
2022-02-28 17:16:31 +0000

    added mouse enter and leave events

--- Week4/e6.js
+++ Week4/e6.js
@@ -9,10 +9,22 @@ function myFunc1(){
     
 
 }
+function myFunc2(){
+    document.getElementById("ele2").firstChild.nodeValue = 'Changed when mouse enter';
+    
+
+}
+function myFunc3(){
+    document.getElementById("ele2").firstChild.nodeValue = 'Changed when mouse Leave';
+    
+
+}
 function myLoadFunc(){
     var element1= document.getElementById("ele1");
     element1.addEventListener('click',myFunc);
     var element2= document.getElementById("ele2");
     element2.addEventListener('click',myFunc1);
+    element2.addEventListener('mouseenter',myFunc2);
+    element2.addEventListener('mouseleave',myFunc3);
 }
 document.addEventListener('DOMContentLoaded',myLoadFunc);
