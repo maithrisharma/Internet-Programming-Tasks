2bdacfb5b0445bee424bb5bef725f7f4f8428a92
Maithri Sharma <maithribharadwaj3@gmail.com>
2022-02-28 16:55:30 +0000

    onclick

---
+++ Week4/e4.js
@@ -0,0 +1,7 @@
+function myFunc(){
+    var e=document.getElementById("ele1");
+    e.firstChild.nodeValue = 'New Heading';
+    document.getElementById("ele2").firstChild.nodeValue="New content"
+
+}
+document.addEventListener('click',myFunc);
