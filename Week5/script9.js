7f11570a48bf34c86776762d5b69c783d42aceac
Maithri Sharma <maithribharadwaj3@gmail.com>
2022-03-10 20:51:12 +0000

    with set interval

--- Week5/script9.js
+++ Week5/script9.js
@@ -3,25 +3,44 @@ function myFunc(){
     document.addEventListener('keydown',clickFunc);
 
 }
-function clickFunc(event){
-    console.log(event.keyCode);
+function moveLeft(){
     var element= document.getElementById("circle");
     var positionLeft=element.offsetLeft;
+    element.style.left=positionLeft-1+'px';
+}
+function moveRight(){
+    var element= document.getElementById("circle");
+    var positionLeft=element.offsetLeft;
+    element.style.left=positionLeft+1+'px';
+}
+function moveTop(){
+    var element= document.getElementById("circle");
     var positionTop=element.offsetTop;
+    element.style.top=positionTop-1+'px';
+}
+function moveBottom(){
+    var element= document.getElementById("circle");
+    var positionTop=element.offsetTop;
+    element.style.top=positionTop+1+'px'
+}
+function clickFunc(event){
+    console.log(event.keyCode);
+    
     if(event.keyCode==37){
-        element.style.left=positionLeft-10+'px';
+        setInterval(moveLeft,10);
+        
     }
     else if(event.keyCode==39){
+        setInterval(moveRight,10);
         
-        element.style.left=positionLeft+10+'px';
     }
     else if(event.keyCode==38){
+        setInterval(moveTop,10);
         
-        element.style.top=positionTop-10+'px';
     }
     else if(event.keyCode==40){
-        
-        element.style.top=positionTop+10+'px';
+        setInterval(moveBottom,10);
+        ;
     }
     
 }
